db5e08da765851514145ff460a7aa448a22637a5
hsejsx <hsejsx@gmail.com>
2022-11-29 13:12:54 +0900

    JS : arrow btn

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 
 const header = document.querySelector('.header');
 const headerHeight = Math.ceil(header.getBoundingClientRect().height);
+const arrowBtn = document.querySelector('.arrow-btn');
 console.log(headerHeight);
 window.addEventListener('scroll', () => {
     if (window.scrollY > headerHeight) {
+        arrowBtn.classList.add('show');
         return;
     } else {
         header.style.backgroundColor = `rgba(255, 255, 255, ${window.scrollY / headerHeight})`;
+        arrowBtn.classList.remove('show');
     }
 })
