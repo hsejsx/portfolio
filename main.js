5c9b428f23e2156eec578303442359693b4da682
hsejsx <hsejsx@gmail.com>
2022-11-29 20:11:44 +0900

    JS : projects

--- main.js
+++ main.js
@@ -13,3 +13,47 @@ window.addEventListener('scroll', () => {
         arrowBtn.classList.remove('show');
     }
 })
+
+const count = document.querySelector('#projects .section__description');
+const categoryList = document.querySelector('.project__category');
+const categories = document.querySelectorAll('.category__list__item button');
+const projectList = document.querySelector('.project__list');
+const projects = document.querySelectorAll('.project__list .project');
+
+printCount();
+
+categoryList.addEventListener('click', (e) => {
+    const target = e.target.dataset.type;
+    if (target) {
+        showProject(target);
+        showActive(e.target);
+    }
+})
+
+function showActive(e) {
+    categories.forEach(category => category.classList.remove('is-active'));
+    console.log(categories);
+    e.classList.add('is-active');
+    console.log(e);
+}
+
+function showProject(type) {
+    projectList.classList.add('animation');
+    setTimeout(() => {
+        projectList.classList.remove('animation');
+        projectList.textContent = '';
+        const projectsArr = Array.from(projects);
+        if (type === 'all') {
+            projectsArr.forEach(project => projectList.appendChild(project));
+        } else {
+            const filtered = projectsArr.filter(project => project.dataset.type === type);
+            filtered.forEach(project => projectList.appendChild(project));
+        }
+        printCount();
+    }, 300);
+}
+
+function printCount() {
+    const projectCount = projectList.childElementCount;
+    count.textContent = `${projectCount} 개의 프로젝트가 있습니다.`;
+}
